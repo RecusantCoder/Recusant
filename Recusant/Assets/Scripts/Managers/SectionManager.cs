20b740ddf96f4655b6bdb97117569b76e9747f9c
Nathan Goddard <127622043+RecusantCoder@users.noreply.github.com>
2023-06-07 15:34:08 -0400

    background and collide objects
    Log and rock added as collidable object in the way, background tiles now has 10 more tiles. They tiles AND COLLIDABLES are randomly spawned and remembers where they are, it disables them and enables them too.

--- Recusant/Assets/Scripts/Managers/SectionManager.cs
+++ Recusant/Assets/Scripts/Managers/SectionManager.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class SectionManager : MonoBehaviour
 {
     public GameObject[] sectionPrefabs;
+    public GameObject[] collidePrefabs;
     private Transform playerTransform;
     private Vector2 spawn = new Vector2(0f, 0f);
     private float sectionLength = 10.24f;
-    //private int numOfSectionsOnScreen = 3;
     private bool startPoint = false;
-    
-    enum SectionDirection {Right, TopRight, Top, TopLeft, Left, BottomLeft, Bottom, BottomRight, Centre}
-    
+    private Dictionary<Vector2, GameObject> spawnedSections = new Dictionary<Vector2, GameObject>();
+
+    enum SectionDirection { Right, TopRight, Top, TopLeft, Left, BottomLeft, Bottom, BottomRight, Centre }
+
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         SpawnSection(SectionDirection.Centre);
-        
+
         SpawnSection(SectionDirection.Right);
-        
+
         SpawnSection(SectionDirection.TopRight);
         SpawnSection(SectionDirection.Top);
         SpawnSection(SectionDirection.TopLeft);
-        
+
         SpawnSection(SectionDirection.Left);
-        
+
         SpawnSection(SectionDirection.BottomLeft);
         SpawnSection(SectionDirection.Bottom);
         SpawnSection(SectionDirection.BottomRight);
-        
     }
 
     // Update is called once per frame
@@ -39,9 +39,39 @@ public class SectionManager : MonoBehaviour
         TrackPlayer();
     }
 
-    //Sets the spawn point of Sections properly as the player moves
+    // Sets the spawn point of Sections properly as the player moves
     private void TrackPlayer()
     {
+        // Calculate the position of the player in section coordinates
+        int playerSectionX = Mathf.FloorToInt(playerTransform.position.x / sectionLength);
+        int playerSectionY = Mathf.FloorToInt(playerTransform.position.y / sectionLength);
+
+        // Iterate through all the spawned sections
+        foreach (var kvp in spawnedSections)
+        {
+            Vector2 sectionPosition = kvp.Key;
+            GameObject section = kvp.Value;
+
+            // Calculate the position of the section in section coordinates
+            int sectionX = Mathf.FloorToInt(sectionPosition.x / sectionLength);
+            int sectionY = Mathf.FloorToInt(sectionPosition.y / sectionLength);
+
+            // Calculate the distance between the player section and the current section
+            int distanceX = Mathf.Abs(playerSectionX - sectionX);
+            int distanceY = Mathf.Abs(playerSectionY - sectionY);
+
+            // Check if the section is within the desired distance range (e.g., radius of 2 tiles)
+            if (distanceX <= 2 && distanceY <= 2)
+            {
+                // Enable the section
+                section.SetActive(true);
+            }
+            else
+            {
+                // Disable the section
+                section.SetActive(false);
+            }
+        }
         if (playerTransform.position.x > (spawn.x + sectionLength))
         {
             spawn.x += sectionLength;
@@ -70,8 +100,6 @@ public class SectionManager : MonoBehaviour
             SpawnSection(SectionDirection.Bottom);
             SpawnSection(SectionDirection.BottomRight);
         }
-        //Debug.Log("X: " + playerTransform.position.x + "  Y: " + playerTransform.position.y);
-        //Debug.Log("SpawnX: " + spawn.x + "  SpawnY: " + spawn.y);
     }
 
     private void SpawnSection(SectionDirection sd)
@@ -84,6 +112,7 @@ public class SectionManager : MonoBehaviour
             spawn = playerTransform.position;
             centre.transform.position = spawn;
             startPoint = true;
+            spawnedSections.Add(spawn, centre);
         }
         else
         {
@@ -91,85 +120,73 @@ public class SectionManager : MonoBehaviour
             switch (sd)
             {
                 case SectionDirection.Right:
-                //right
-                    GameObject right;
-                    right = Instantiate(sectionPrefabs[0]) as GameObject;
-                    right.transform.SetParent(transform);
+                    // right
                     spawn.x += sectionLength;
-                    right.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
                 case SectionDirection.TopRight:
-                    //top right
-                    GameObject topRight;
-                    topRight = Instantiate(sectionPrefabs[0]) as GameObject;
-                    topRight.transform.SetParent(transform);
+                    // top right
                     spawn.x += sectionLength;
                     spawn.y += sectionLength;
-                    topRight.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
                 case SectionDirection.Top:
-                    //top
-                    GameObject top;
-                    top = Instantiate(sectionPrefabs[0]) as GameObject;
-                    top.transform.SetParent(transform);
+                    // top
                     spawn.y += sectionLength;
-                    top.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
                 case SectionDirection.TopLeft:
-                    //top left
-                    GameObject topLeft;
-                    topLeft = Instantiate(sectionPrefabs[0]) as GameObject;
-                    topLeft.transform.SetParent(transform);
+                    // top left
                     spawn.x -= sectionLength;
                     spawn.y += sectionLength;
-                    topLeft.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
                 case SectionDirection.Left:
-                    //left
-                    GameObject left;
-                    left = Instantiate(sectionPrefabs[0]) as GameObject;
-                    left.transform.SetParent(transform);
+                    // left
                     spawn.x -= sectionLength;
-                    left.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
                 case SectionDirection.BottomLeft:
-                    //bottom left
-                    GameObject bottomLeft;
-                    bottomLeft = Instantiate(sectionPrefabs[0]) as GameObject;
-                    bottomLeft.transform.SetParent(transform);
+                    // bottom left
                     spawn.x -= sectionLength;
                     spawn.y -= sectionLength;
-                    bottomLeft.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
                 case SectionDirection.Bottom:
-                    //bottom
-                    GameObject bottom;
-                    bottom = Instantiate(sectionPrefabs[0]) as GameObject;
-                    bottom.transform.SetParent(transform);
+                    // bottom
                     spawn.y -= sectionLength;
-                    bottom.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
-                case SectionDirection.BottomRight:    
-                    //bottom right
-                    GameObject bottomRight;
-                    bottomRight = Instantiate(sectionPrefabs[0]) as GameObject;
-                    bottomRight.transform.SetParent(transform);
+                case SectionDirection.BottomRight:
+                    // bottom right
                     spawn.x += sectionLength;
                     spawn.y -= sectionLength;
-                    bottomRight.transform.position = spawn;
-                    spawn = spawnTemp;
                     break;
                 default:
                     Debug.Log("SectionDirection Default");
                     break;
             }
+
+            if (spawnedSections.ContainsKey(spawn))
+            {
+                GameObject section = spawnedSections[spawn];
+                section.SetActive(true);
+            }
+            else
+            {
+                GameObject randomSectionPrefab = sectionPrefabs[Random.Range(0, sectionPrefabs.Length)];
+                GameObject spawnedSection = Instantiate(randomSectionPrefab) as GameObject;
+                spawnedSection.transform.SetParent(transform);
+                spawnedSection.transform.position = spawn;
+                spawnedSections.Add(spawn, spawnedSection);
+                
+                // Spawn 2-5 random collidePrefabs within the area of the sectionPrefab
+                int numberOfCollidePrefabs = Random.Range(2, 6);
+                for (int i = 0; i < numberOfCollidePrefabs; i++)
+                {
+                    GameObject randomCollidePrefab = collidePrefabs[Random.Range(0, collidePrefabs.Length)];
+                    GameObject spawnedCollidePrefab = Instantiate(randomCollidePrefab) as GameObject;
+                    spawnedCollidePrefab.transform.SetParent(spawnedSection.transform);
+                    // Set the position of the spawned collidePrefab within the area of the sectionPrefab
+                    Vector2 randomPosition = new Vector2(Random.Range(-sectionLength / 2f, sectionLength / 2f), Random.Range(-sectionLength / 2f, sectionLength / 2f));
+                    spawnedCollidePrefab.transform.localPosition = randomPosition;
+                }
+            }
+
+            spawn = spawnTemp;
         }
     }
 }
