d7069610f655f567f418ce48dd39f2159bd66d8a
Nathan Goddard <127622043+RecusantCoder@users.noreply.github.com>
2023-06-10 12:59:48 -0400

    Orb attaction
    Orb class now moves to the player and is destroyed on pickup, player pickup radius is increased

--- Recusant/Assets/Scripts/ItemScripts/Orb.cs
+++ Recusant/Assets/Scripts/ItemScripts/Orb.cs
@@ -6,18 +6,43 @@ public class Orb : Interactable
 {
     public float xpDrop;
     public string thisName;
-    
+    private GameObject _player;
+    private bool isPickedUp = false;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
     public override void Interact()
     {
         base.Interact();
-        PickUp();
+        if (!isPickedUp)
+        {
+            isPickedUp = true;
+            PickUp();
+        }
     }
 
     void PickUp()
     {
+        StartCoroutine(MoveToPlayerAndDestroy());
+    }
+    
+    IEnumerator MoveToPlayerAndDestroy()
+    {
+        Transform playerTransform = _player.transform;
+        float speed = 2f; // Adjust the speed at which the object moves towards the player
+        
+        while (Vector3.Distance(transform.position, playerTransform.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+            yield return null;
+        }
+        
         LevelBar.instance.AddExperience(xpDrop);
         FindObjectOfType<AudioManager>().Play("pickup");
+        
         Destroy(gameObject);
-
     }
 }
