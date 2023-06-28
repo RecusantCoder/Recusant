300274acc8aa4191e1c72013dcdf79964c8630d3
Nathan Goddard <127622043+RecusantCoder@users.noreply.github.com>
2023-06-28 14:54:06 -0400

    Syrup added
    New powerup syrup uses food script and similar to orb object and script. Shooting for shotgun and lazer are only aiming up needs fixing.

---
+++ Recusant/Assets/Scripts/ItemScripts/Food.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : Interactable
+{
+    public float healAmount;
+    public string thisName;
+    private GameObject _player;
+    private bool isPickedUp = false;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (!isPickedUp)
+        {
+            isPickedUp = true;
+            PickUp();
+        }
+    }
+
+    void PickUp()
+    {
+        StartCoroutine(MoveToPlayerAndDestroy());
+    }
+    
+    IEnumerator MoveToPlayerAndDestroy()
+    {
+        Transform playerTransform = _player.transform;
+        float speed = 2f; // Adjust the speed at which the object moves towards the player
+        
+        while (Vector3.Distance(transform.position, playerTransform.position) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+            yield return null;
+        }
+        
+        //Heal to max health, not to healAmount
+        PlayerManager.instance.player.GetComponent<PlayerStats>().currentHealth =
+            PlayerManager.instance.player.GetComponent<PlayerStats>().maxHealth;
+        
+        FindObjectOfType<AudioManager>().Play("pickup");
+        
+        Destroy(gameObject);
+    }
+}
